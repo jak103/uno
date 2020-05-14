5ca44aa13d58454d502fd4f0838198c929d192b1
Jacob Christensen <jacob.h.christensen@gmail.com>
2020-05-14 10:02:15 -0600

    Initial echo server

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jak103/usu-uno
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	fmt.Println("USU - UNO v0.0.0")
+
+	// New Echo server
+	e := echo.New()
+
+	// Setup middleware
+	e.File("/", "static/index.html")
+	//e.Use(middleware.Static("/static")) TODO: Figure out how to serve the statis js files
+	e.Use(middleware.Gzip())
+	e.Use(middleware.Recover())
+
+	// Setup routes
+	setupRoutes(e)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
