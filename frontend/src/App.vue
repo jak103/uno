6153da9b0c986ad3b69bc7ff945d5e857fb39c7d
Ryan Johnson <ryan.johnson.1315@gmail.com>
2020-05-14 23:42:00 -0600

    Initial frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-content>
+      <!-- Imports the login. See index.js -->
+      <v-btn to="/">Login</v-btn>
+      <router-view/> 
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+// import axios from 'axios';
+export default {
+  name: 'App',
+
+  components: {
+  },
+  data: () => ({
+    //
+  }),
+  methods: {
+    //
+  }
+};
+</script>
---
+++ frontend/src/views/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>Current Game id: {{ game_id }}</h2>
+    <div v-if="valid">
+      Game is valid!
+    </div>
+    <div v-else>
+      Game is invalid
+    </div>
+  </div>
+</template>
+
+<script>
+// This will eventually be the game page
+  export default {
+    props: {
+      game_id: {
+        type: String,
+        required: true
+      },
+      valid: {
+        type: Boolean,
+        required: true
+      }
+    },
+  };
+</script>
