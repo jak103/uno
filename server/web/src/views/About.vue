39f88e62950f09f4fde50bbf701a740abebe85db
Ryan Johnson <ryan.johnson.1315@gmail.com>
2020-05-18 19:58:30 -0600

    Polling is working

--- server/web/src/views/About.vue
+++ server/web/src/views/About.vue
@@ -1,40 +1,37 @@
 <template>
   <div>
     <v-container>
-      <v-row :class="mb-6">
-        <v-col>
+      <v-row :class="'mb-6'">
+        <v-col :cols="6">
           <!-- Game stats -->
           <v-row>
-            <v-card class="ma-3 pa-6" outlined tile>
+            <v-card :class="'ma-3 pa-6'" outlined tile>
               Current Game id: {{ game_id }}
               <span v-if="!valid">Invalid Game Id</span>
             </v-card>
-            <v-card class="ma-3 pa-6" outlined tile>
+            <v-card :class="'ma-3 pa-6'" outlined tile>
               <v-btn v-if="valid" @click.native="startGame">Start Game</v-btn>
               <v-btn v-else to="/">Create a new game</v-btn>
             </v-card>
           </v-row>
           <!-- Game Players -->
-          <v-row caption="Current Players">
+          <v-row>
             <v-card
               v-for="player in players"
               :key="player"
               color="current_player == player ? '#1F7087' : ''"
-              class="ma-3 pa-6"
+              :class="'ma-3 pa-6'"
               outlined
               tile
             >
               {{ player }}
-              <span v-if="current_player == player">
-                <br />On clock
-              </span>
             </v-card>
           </v-row>
           <!-- Current Card and actions -->
           <v-row v-if="current_player != ''">
-            <v-card class="ma-3 pa-6" outlined tile>
+            <v-card :class="'ma-3 pa-6'" outlined tile>
               Current Card
-              <br>
+              <br />
               <Card
                 v-for="card in current_card"
                 :number="card.number"
@@ -42,7 +39,7 @@
                 :color="card.color"
               />
             </v-card>
-            <v-card class="ma-3 pa-6" outlined tile>
+            <v-card :class="'ma-3 pa-6'" outlined tile v-if="current_player == this_user">
               <v-btn>Take from pile</v-btn>
               <br />
               <br />or
@@ -50,12 +47,20 @@
               <br />
               <v-btn>Draw from deck</v-btn>
             </v-card>
+            <v-card v-else :class="'ma-3 pa-6'" outlined tile>
+              Waiting for {{ current_player }} to play
+            </v-card>
           </v-row>
         </v-col>
-        <v-col class="mb-6" v-if="current_card != ''">
-          <v-card>Click to play a card from your hand</v-card>
-          <br />
-          <Card v-for="(card, i) in cards" :key="i" :number="card.number" :color="card.color" @click.native="playCard"></Card>
+        <v-col :class="'mb-6'" v-if="current_card != ''">
+          <v-card :class="'ma-3 pa-6'" outlined tile>Click to play a card from your hand</v-card>
+          <Card
+            v-for="(card, i) in cards"
+            :key="i"
+            :number="card.number"
+            :color="card.color"
+            @click.native="playCard"
+          ></Card>
         </v-col>
       </v-row>
     </v-container>
@@ -68,11 +73,11 @@ import Card from "../components/Card";
 export default {
   props: {
     game_id: {
-      required: true
+      required: false
     },
     valid: {
       type: Boolean,
-      required: true
+      required: false
     }
   },
   data() {
@@ -80,26 +85,37 @@ export default {
       cards: [],
       current_player: "",
       players: [],
-      current_card: []
+      current_card: [],
+      this_user: "Ryan"
     };
   },
   components: {
     Card
   },
   methods: {
-    async startGame() {
-      let res = await axios.post("/startgame/" + this.game_id);
-      if (res.data.valid) {
-        this.valid = res.data.valid;
-        this.cards = res.data.payload.cards;
-        this.current_player = res.data.payload.current_player;
-        this.players = res.data.payload.players;
-        this.current_card = res.data.payload.current_card;
-      }
+    updateData() {
+      axios.get("/update/" + this.game_id).then(res => {
+        if (res.data.valid) {
+          this.valid = res.data.valid;
+          this.cards = res.data.payload.cards;
+          this.current_player = res.data.payload.current_player;
+          this.players = res.data.payload.players;
+          this.current_card = res.data.payload.current_card;
+        }
+        console.log("Updating");
+      });
+    },
+    startGame() {
+      this.updateData();
     },
     playCard() {
-      console.log("Playing card!")
+      console.log("Playing card!");
     }
+  },
+  created() {
+    setInterval(() => {
+      this.updateData();
+    }, 2000);
   }
 };
 </script>
