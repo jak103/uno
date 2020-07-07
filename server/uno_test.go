8bf9d92e28e472e8fb83861e22525045e2f19a10
gitdaddy <marsden.daniel@gmail.com>
2020-07-07 09:44:50 -0500

    test default payload

--- server/uno_test.go
+++ server/uno_test.go
@@ -16,25 +16,23 @@ func TestColor(t *testing.T) {
 	r1 := randColor(0)
 	r2 := ""
 
-	assert.NotEqual(r1, r2, "The two colors should be the same.")
+	// expected, actual, msg
+	assert.NotEqual(r2, r1, "The two colors should be the same.")
 }
 
-func TestPayload(t *testing.T) {
+func TestDefaultPayload(t *testing.T) {
 	fmt.Println("Running Payload Test")
+	assert := assert.New(t)
 
-	// nilPayload := newPayload("user_test")
+	nilPayload := newPayload("user_test")
 
-	// payload["current_card"] = currCard
-	// payload["current_player"] = currPlayer
-	// payload["all_players"] = players
-	// payload["deck"] = allCards[user] // returns nil if currPlayer = "" or user not in allCards
-	// payload["game_id"] = gameID
-	// payload["game_over"] = checkForWinner()
-	// var emptyCardArray []Card
-	// assert.Equal(nilPayload["current_card"], emptyCardArray, "Card should be nil")
-	// assert.Equal(nilPayload["current_player"], nil, "Player should be nil")
-	// assert.Equal(nilPayload["all_players"], nil, "All players should be nil")
-	// assert.Equal(nilPayload["deck"], nil, "Card should be nil")
-	// assert.Equal(nilPayload["game_id"], nil, "Card should be nil")
-	// assert.Equal(nilPayload["game_over"], nil, "Card should be nil")
+	var emptyCardArray []Card
+	var emptyPlayerArray []string = []string{}
+
+	assert.Equal(emptyCardArray, nilPayload["current_card"], "Current card should be empty")
+	assert.Equal("", nilPayload["current_player"], "Player should be empty")
+	assert.Equal(emptyPlayerArray, nilPayload["all_players"], "Players list should be empty")
+	assert.Equal(emptyCardArray, nilPayload["deck"], "Card should be empty")
+	assert.Equal("", nilPayload["game_id"], "Game id should be empty")
+	assert.Equal("", nilPayload["game_over"], "Game over should be empty")
 }
