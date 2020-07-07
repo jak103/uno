dc45108545fc7fcec9847d1fe6bedfbc8a460a1c
Ithilendil <38262817+Ithilendil@users.noreply.github.com>
2020-07-07 16:12:11 -0600

    Update uno_testing.go
    Adding in the first test for testing the function RandColor in uno.go

---
+++ server/uno_testing.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+/*Example formatting of a test from class sample code
+func TestSubtract(t *testing.T) {
+	assert.Equal(t, 6, subtract(9, 3))
+}
+*/
+
+/******************************************************************************
+This function will test the function randColor in uno.go
+		randColor can return "red", "blue", "green", "yellow" or ""
+******************************************************************************/
+func TestRandColor(t *testing.T) {
+	
+	
+
+}
