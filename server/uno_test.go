afeb815f1e9d1d94836b67eab3d8a0dba722525e
gitdaddy <marsden.daniel@gmail.com>
2020-07-07 09:24:37 -0500

    Rand color test

--- server/uno_test.go
+++ server/uno_test.go
@@ -3,17 +3,38 @@ package main
 import (
 	"fmt"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -cover
 
-func TestUno(t *testing.T)  {
-	fmt.Println("Running Uno Test...");
-	assert := assert.New(t) // for multiple asserts
+func TestColor(t *testing.T) {
+	fmt.Println("Running Rand Color Test")
+	assert := assert.New(t)
 
-	a := "Hello"
-	b := "Hello"
+	r1 := randColor(0)
+	r2 := ""
 
-	assert.Equal(a, b, "The two words should be the same.")
+	assert.NotEqual(r1, r2, "The two colors should be the same.")
+}
+
+func TestPayload(t *testing.T) {
+	fmt.Println("Running Payload Test")
+
+	// nilPayload := newPayload("user_test")
+
+	// payload["current_card"] = currCard
+	// payload["current_player"] = currPlayer
+	// payload["all_players"] = players
+	// payload["deck"] = allCards[user] // returns nil if currPlayer = "" or user not in allCards
+	// payload["game_id"] = gameID
+	// payload["game_over"] = checkForWinner()
+	// var emptyCardArray []Card
+	// assert.Equal(nilPayload["current_card"], emptyCardArray, "Card should be nil")
+	// assert.Equal(nilPayload["current_player"], nil, "Player should be nil")
+	// assert.Equal(nilPayload["all_players"], nil, "All players should be nil")
+	// assert.Equal(nilPayload["deck"], nil, "Card should be nil")
+	// assert.Equal(nilPayload["game_id"], nil, "Card should be nil")
+	// assert.Equal(nilPayload["game_over"], nil, "Card should be nil")
 }
