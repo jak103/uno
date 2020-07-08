94c8b5f6c38e592cdf97326dba8b80eba4e4344a
Andrew McMullin <mcmullinboy15@gmail.com>
2020-07-07 22:47:23 -0600

    started tests

--- server/routeHandler_test.go
+++ server/routeHandler_test.go
@@ -23,3 +23,45 @@ func TestNewGame(t *testing.T) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 	}
 }
+
+//	"http://localhost:8080/play/" +
+//	this.game_id +
+//	"/" +
+//	this.username +
+//	"/" +
+//	card.number +
+//	"/" +
+//	card.color
+
+func ValidCardTest(t *testing.T) {
+
+	e := echo.New()
+	setupRoutes(e)
+	req := httptest.NewRequest(http.MethodPost, "/newgame", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, )
+}
+
+// Make sure that the Username is Set
+// Make sure that the Usernames accross computers are not overlapping
+func UserNameTest(t *testing.T) {
+
+	e := echo.New()
+	setupRoutes(e)
+	req := httptest.NewRequest(http.MethodPost, "/newgame", nil)
+
+	if assert.NoError(t, )
+}
+
+func YourTurnTest(t *testing.T) {
+
+
+}
+
+func WonTheGameTest(t *testing.T) {
+
+
+}
