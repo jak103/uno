b4ea415f3c2bd08f8a9c4697c6fa8405321f1712
Garrett Warnock <garrett.savage.warnock@aggiemail.usu.edu>
2020-07-07 22:04:36 -0600

    Added unit tests with increase by 10%. It would be nice to do more and make the existing tests look better. Try to understand what echo is...

--- server/uno_test.go
+++ server/uno_test.go
@@ -1,25 +1,41 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
+	"fmt"
 	"testing"
 
-	"github.com/labstack/echo"
 	"github.com/stretchr/testify/assert"
+	// "github.com/labstack/echo"
 )
 
-func TestNewGame(t *testing.T) {
+func TestRandColor(t *testing.T) {
 	// Setup
-	e := echo.New()
-	setupRoutes(e)
-	req := httptest.NewRequest(http.MethodPost, "/newgame", nil)
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-
-	// Assertions
-	if assert.NoError(t, newGame(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-	}
+	assert.Equal(t,randColor(0),"red")
+	assert.Equal(t,randColor(1),"blue")
+	assert.Equal(t,randColor(2),"green")
+	assert.Equal(t,randColor(3),"yellow")
 }
+
+func TestCheckID(t *testing.T) {
+	fmt.Print("hello\n")
+	assert.Equal(t,checkID("user"),bool(false))
+}
+
+func TestContains(t *testing.T){
+	test := []string {"number", "reverse", "wild", "plus two"}
+	index, isItThere := contains(test,"number")
+	assert.Equal(t, index, 0)
+	assert.Equal(t,isItThere,bool(true))
+}
+
+func TestNewRandomCard(t *testing.T){
+	test := newRandomCard() 
+
+	assert.Greater(t, test[0].Number, -1)
+	assert.Less(t,test[0].Number,10)
+}
+
+// func TestCreateNewGame(t *testing.T){
+// 	test := createNewGame(e)
+// 	fmt.Print(test)
+// }
