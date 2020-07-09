a82f35da19c35f5ebcea718e513e4d3254c148c3
Jason Boyd <jasonboyd99@gmail.com>
2020-07-08 20:37:22 -0600

    Added play unit test

--- server/routeHandler_test.go
+++ server/routeHandler_test.go
@@ -51,3 +51,16 @@ func TestUpdate(t *testing.T) {
         assert.Equal(t, http.StatusOK, rec.Code)
     }
 }
+
+func TestPlay(t *testing.T) {
+    e := echo.New()
+    setupRoutes(e)
+    req := httptest.NewRequest(http.MethodPost, "/play", nil)
+    req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+    rec := httptest.NewRecorder()
+    c := e.NewContext(req, rec)
+
+    if assert.NoError(t, play(c)) {
+        assert.Equal(t, http.StatusOK, rec.Code)
+    }
+}
