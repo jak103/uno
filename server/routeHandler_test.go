97d4eadd28225fae5daa7ce21be75f45c72098e2
Jason Boyd <jasonboyd99@gmail.com>
2020-07-08 20:35:42 -0600

    Added login unit test

--- server/routeHandler_test.go
+++ server/routeHandler_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Route handlers unit tests
+
 func TestNewGame(t *testing.T) {
 	// Setup
 	e := echo.New()
@@ -20,6 +22,19 @@ func TestNewGame(t *testing.T) {
 
 	// Assertions
 	if assert.NoError(t, newGame(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
+
+func TestLogin(t *testing.T) {
+	e := echo.New()
+	setupRoutes(e)
+	req := httptest.NewRequest(http.MethodPost, "/login", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, login(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 	}
 }
