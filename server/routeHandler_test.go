5e11fe68c00fd6e464d4e6bff3a9a70010169f65
Alex Rasmussen <alex.rasmussen@aggiemail.usu.edu>
2020-07-08 23:36:59 -0600

    Added test for Login

--- server/routeHandler_test.go
+++ server/routeHandler_test.go
@@ -23,3 +23,18 @@ func TestNewGame(t *testing.T) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 	}
 }
+
+func TestLogin(t *testing.T) {
+	// Setup
+	e := echo.New()
+	setupRoutes(e)
+	req := httptest.NewRequest(http.MethodPost, "/login/:game/:username", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Assertions
+	if assert.NoError(t, login(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
