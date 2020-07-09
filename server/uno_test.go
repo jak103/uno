4cd09da077c13ab023ce91cac17a967f242e4574
Hunter Meredith <hunterjmery@gmail.com>
2020-07-09 16:58:54 -0600

    finished

---
+++ server/uno_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestrandColor(t *testing.T) {
+	assert.Equal(t, randColor(0), "red")
+	assert.Equal(t, randColor(1), "blue")
+	assert.Equal(t, randColor(2), "green")
+	assert.Equal(t, randColor(3), "yellow")
+}
