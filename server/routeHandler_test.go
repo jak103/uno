6e59cbc755362c0ded37454bad509c21d11b0eea
Alex Rasmussen <alex.rasmussen@aggiemail.usu.edu>
2020-07-08 23:56:04 -0600

    Added test for Update and Draw route handlers

--- server/routeHandler_test.go
+++ server/routeHandler_test.go
@@ -24,6 +24,21 @@ func TestNewGame(t *testing.T) {
 	}
 }
 
+func TestUpdate(t *testing.T) {
+	// Setup
+	e := echo.New()
+	setupRoutes(e)
+	req := httptest.NewRequest(http.MethodPost, "/update/:game/:username", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Assertions
+	if assert.NoError(t, update(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
+
 func TestLogin(t *testing.T) {
 	// Setup
 	e := echo.New()
@@ -35,6 +50,21 @@ func TestLogin(t *testing.T) {
 
 	// Assertions
 	if assert.NoError(t, login(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
+
+func TestDraw(t *testing.T) {
+	// Setup
+	e := echo.New()
+	setupRoutes(e)
+	req := httptest.NewRequest(http.MethodPost, "/draw/:game/:username", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Assertions
+	if assert.NoError(t, draw(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 	}
 }
