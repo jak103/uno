fb387284172675e179b4356d96b4cf260017e3d5
Logan Hendricks <logan.hendricks@digicert.com>
2020-07-09 19:06:38 -0600

    Revert install process to match dev

--- client/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-UNAME=$(uname)
-if [[ "${UNAME}" == CYGWIN* || "${UNAME}" == MINGW* || "${UNAME}" == MSYS* ]]; then
-  # If using Git Bash on Windows, it will attempt to transform paths to windows Paths. Setting MSYS_NO_PATHCONV stops this process
-  export MSYS_NO_PATHCONV=1
-fi
-
-docker run --rm -v ${PWD}:/client -w /client node:12-slim bash -c "npm install"
