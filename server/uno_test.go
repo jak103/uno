9a5450b87413faf2da8fb9e8351d330a0d2dbf9c
Brent Buffenbarger <59784636+brent-buffenbarger@users.noreply.github.com>
2020-07-13 15:18:26 -0600

    Update uno_test.go

--- server/uno_test.go
+++ server/uno_test.go
@@ -6,6 +6,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// test to make sure the correct colors are being returned from RandColor
+// this test is not super helpful - it is being used to get used to unit testing in golang
+func TestRandColor(t *testing.T) {
+	assert.Equal(t, randColor(0), "red")
+	assert.Equal(t, randColor(1), "blue")
+	assert.Equal(t, randColor(2), "green")
+	assert.Equal(t, randColor(3), "yellow")
+	assert.Equal(t, randColor(4), "")
+}
+
 func TestContains(t *testing.T) {
 	// setup test data
 	var data []string = []string{}
