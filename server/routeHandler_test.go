8d26f77586967ff116e20586ec084d988bf13303
Nathan Johnson <9ncjohnson@gmail.com>
2020-07-14 12:25:26 -0600

    added some more tests

--- server/routeHandler_test.go
+++ server/routeHandler_test.go
@@ -28,7 +28,7 @@ func TestUpdate(t *testing.T) {
 	// Setup
 	e := echo.New()
 	setupRoutes(e)
-	req := httptest.NewRequest(http.MethodPost, "/update/:game/:username", nil)
+	req := httptest.NewRequest(http.MethodPost, "/update/:12234/:testUser", nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
@@ -43,7 +43,7 @@ func TestLogin(t *testing.T) {
 	// Setup
 	e := echo.New()
 	setupRoutes(e)
-	req := httptest.NewRequest(http.MethodPost, "/login/:game/:username", nil)
+	req := httptest.NewRequest(http.MethodPost, "/login/:12234/:testUser", nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
@@ -58,7 +58,7 @@ func TestPlay(t *testing.T) {
 	// Setup
 	e := echo.New()
 	setupRoutes(e)
-	req := httptest.NewRequest(http.MethodPost, "/play/:game/:username/:number/:color", nil)
+	req := httptest.NewRequest(http.MethodPost, "/play/:12234/:testUser/:1/:red", nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
@@ -73,13 +73,35 @@ func TestDraw(t *testing.T) {
 	// Setup
 	e := echo.New()
 	setupRoutes(e)
-	req := httptest.NewRequest(http.MethodPost, "/draw/:game/:username", nil)
+	req := httptest.NewRequest(http.MethodPost, "/draw/:12234/:testUser", nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
 	// Assertions
 	if assert.NoError(t, draw(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
+
+func TestCheckForWinner(t *testing.T) {
+	players = []string{"testUser"}
+	allCards = make(map[string][]Card)
+	dealCards()
+	assert.Equal(t, "", checkForWinner())
+	allCards[players[0]] = make([]Card, 0)
+	assert.Equal(t, "testUser", checkForWinner())
+}
+
+func TestStartGame(t *testing.T) {
+	e := echo.New()
+	setupRoutes(e)
+	req := httptest.NewRequest(http.MethodPost, "/startgame/12234/testUser", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, login(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 	}
 }
