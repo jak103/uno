12116c637b89daec909e1fe850f672076f0b5e37
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2020-07-21 21:15:13 -0600

    MockDB can have multiple games running at once

--- server/db/MockDB.go
+++ server/db/MockDB.go
@@ -6,17 +6,25 @@ import (
 )
 
 // MockDB is an implemenation declaring the unit test db
-type mockDB struct{}
+type mockDB struct{
+	games []model.Game
+}
 
 // HasGame checks to see if a game with the given ID exists in the database.
 func (db *mockDB) HasGame(game string) bool {
-	return game == "12234"
+	for i, myGame := range db.games {
+		if myGame.ID == game {
+			return true
+		}
+	}
+	return false
 }
 
 // CreateGame a game with the given ID. Perhaps this should instead just return an id?
-func (db *mockDB) CreateGame(id uuid.UUID) model.Game {
+func (db *mockDB) CreateGame(id uuid.UUID) uuid.UUID {
 	myGame := model.Game{ID: uuid.Nil, Password: "12234"}
-	return myGame
+	db = append(db.games, myGame)
+	return myGame.ID
 }
 
 // LookupGame looks up an existing game in the database.
