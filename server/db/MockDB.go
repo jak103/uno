05a2cb87c035833890b5682a6e0a06828ca2a470
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2020-07-22 13:50:06 -0600

    working on MockDB functions (hasGameById, CreateGame, and LookupGameByID)

--- server/db/MockDB.go
+++ server/db/MockDB.go
@@ -8,15 +8,32 @@ import (
 // MockDB is an implemenation declaring the unit test db
 type mockDB struct {
 	games []model.Game
+	uri string
+	players []string
 }
+
+
+//type mongoDB struct {
+//	client   *mongo.Client
+//	uri      string
+//	database *mongo.Database
+//	games    *mongo.Collection
+//	players  *mongo.Collection
+//}
 
 // HasGame checks to see if a game with the given ID exists in the database.
 func (db *mockDB) HasGameByPassword(password string) bool {
 	return password == "12234"
 }
 
+
 func (db *mockDB) HasGameByID(id string) bool {
-	return true
+	for i, myGame := range db.games {
+		if myGame.ID == id {
+			return true
+		}
+	}
+	return false
 }
 
 // CreateGame a game with the given ID. Perhaps this should instead just return an id?
@@ -28,8 +45,11 @@ func (db *mockDB) CreateGame() (*model.Game, error) {
 
 // LookupGame looks up an existing game in the database.
 func (db *mockDB) LookupGameByID(id string) (*model.Game, error) {
-	myGame := model.Game{ID: uuid.Nil.String(), Password: "12234"}
-	return &myGame, nil
+	for i, myGame := range db.games {
+		if myGame.ID == id {
+			return &myGame, nil
+		}
+	}
 }
 
 // LookupGame looks up an existing game in the database.
