fe2ca1a408856ab4859e05b8463c046ffab19570
Aaron Fine <Aaron.Fine@sdl.usu.edu>
2020-07-23 17:18:47 -0600

    Add helper functions for getCardCount and flesh out logic with dummy data

--- server/gameData.go
+++ server/gameData.go
@@ -1,10 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type HandInfo struct {
+	PlayerName string `json:"name"`
+	CardCount  int    `json:"numCards"`
+}
 
 func getCardCount(gameID string) (string, error) {
-	if gameID == "errortest" {
-		return "invalid data", fmt.Errorf("There was an error")
+
+	var result []*HandInfo = []*HandInfo{}
+
+	players, err := getPlayersInGame(gameID)
+	if err != nil {
+		return "", fmt.Errorf("Could not get list of players in %s: %v", gameID, err)
 	}
-	return "this is a test", nil
+	for _, player := range players {
+		playerHand, err := getHandForPlayerInGame(gameID, player)
+		if err != nil {
+			return "", fmt.Errorf("Could not get hand for player '%s' in game %s: %v", player, gameID, err)
+		}
+
+		result = append(result, &HandInfo{player, len(playerHand)})
+	}
+
+	jsonResult, _ := json.Marshal(result)
+
+	return string(jsonResult), nil
+}
+
+func getPlayersInGame(gameID string) ([]string, error) {
+	// TODO: Remove error test
+	if gameID == "errortest" {
+		return []string{"getPlayersInGame"}, fmt.Errorf("There was an error in getPlayersInGame")
+	}
+
+	// TODO: Remove dummy data
+	return []string{"player 1", "player 2", "player 3"}, nil
+}
+
+func getHandForPlayerInGame(gameID string, playerID string) ([]Card, error) {
+	// TODO: Remove error test
+	if gameID == "errortest" {
+		return []Card{Card{-1, "getHandForPlayerInGame"}}, fmt.Errorf("There was an error in getHandForPlayerInGame")
+	}
+
+	// TODO: Remove dummy data
+	if playerID == "player 1" {
+		return []Card{newRandomCard()[0], newRandomCard()[0], newRandomCard()[0]}, nil
+	}
+	if playerID == "player 2" {
+		return []Card{newRandomCard()[0]}, nil
+	}
+
+	return []Card{}, nil
 }
