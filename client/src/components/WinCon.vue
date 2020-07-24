76e5b493ba64fdda3add9c3e5a7dff400ff097c3
Ryan Goodman <ryan.goodman@aggiemail.usu.edu>
2020-07-24 09:05:37 -0600

    added vue componenet

---
+++ client/src/components/WinCon.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="myApplication">
+    <h1>Hello World</h1>
+    <p>little text behind the title</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
