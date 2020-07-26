e8caabd04fa5c8b7096d636b1bff3b6d4566c67d
Brianna King <brianna.king@aggiemail.usu.edu>
2020-07-25 20:25:33 -0600

    small cosmetic changes

--- client/src/views/Help.vue
+++ client/src/views/Help.vue
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <template class="Scroll">
 
     <div class="Scroll">
@@ -41,6 +42,24 @@
                     <br><strong>Wild Swap Hands Card</strong> – This is a powerful card that enables you to swap the cards in your hand with anyone of your choice. Being a Wild card, it has the same attributes, namely, you can choose to either play it or not, at any of your turns, and also choose the color of play. Ideally, you would probably want to swap your cards with the player who has the least number of cards! Being a powerful Action card, only one is provided per deck. If turned up at the beginning of the game, the first player to start gets to choose the color and also swap his hands with another person. Note: Being a Wild card, the same rule applies to the Swap Hands card. Once you play this card and swapped your hands with another player, your turn is up, and you are not allowed to put down any card immediately after that until your turn arrives again.<br>
                     </p></a>
             </div>
+=======
+<template>
+  <div>
+    <v-container>
+      <!-- <v-row :class="'mb-6'">
+        <v-col :cols="6"> -->
+            <h1 class=text >The first player is normally the player to the left of the dealer (you can also choose the youngest player) and gameplay usually follows a clockwise direction. Every player views his/her cards and tries to match the card in the Discard Pile.
+                You have to match either by the number, color, or the symbol/Action. For instance, if the Discard Pile has a red card that is an 8 you have to place either a red card or a card with an 8 on it. You can also play a Wild card (which can alter current color in play).
+                If the player has no matches or they choose not to play any of their cards even though they might have a match, they must draw a card from the Draw pile. If that card can be played, play it. Otherwise, keep the card, and the game moves on to the next person in turn. You can also play a Wild card, or a Wild Draw Four card on your turn.
+                Note: If the first card turned up from the Draw Pile (to form the Discard Pile) is an Action card, the Action from that card applies and must be carried out by the first player to go (as stated, it is usually the player to the dealer’s left). The exceptions are if a Wild or Wild Draw Four card is turned up.
+                If it is a Wild card, Mattel has now stated that the first player to start (usually the one on the dealer’s left), can choose whatever color to begin play. If the first card is a Wild Draw Four card – Return it to the Draw Pile, shuffle the deck, and turn over a new card. At any time during the game, if the Draw Pile becomes depleted and no one has yet won the round, take the Discard Pile, shuffle it, and turn it over to regenerate a new Draw Pile.
+                Take note that you can only put down one card at a time; you cannot stack two or more cards together on the same turn. For example, you cannot put down a Draw Two on top of another Draw Two, or Wild Draw Four during the same turn, or put down two Wild Draw Four cards together.
+                The game continues until a player has one card left. The moment a player has just one card they must yell “UNO!”. If they are caught not saying “Uno” by another player before the next player has taken their turn, that player must draw two new cards as a penalty. Assuming that the player is unable to play/discard their last card and needs to draw, but after drawing, is then able to play/discard that penultimate card, the player has to repeat the action of calling out “Uno”. The bottom line is – Announcing “Uno” needs to be repeated every time you are left with one card.
+                Once a player has no cards remaining, the game round is over, points are scored, and the game begins over again. Normally, everyone tries to be the first one to achieve 500 points, but you can also choose whatever points number to win the game, as long as everyone agrees to it.
+            </h1>
+        <!-- </v-col>
+        </v-row> -->
+>>>>>>> small cosmetic changes
         </v-container>
     </div>        
 </template>
