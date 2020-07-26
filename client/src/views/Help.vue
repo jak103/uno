1e05ea2970140b0a03e4d17ff15b076b955627b5
Brianna King <brianna.king@aggiemail.usu.edu>
2020-07-25 20:25:33 -0600

    small cosmetic changes

--- client/src/views/Help.vue
+++ client/src/views/Help.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <v-container>
-      <v-row :class="'mb-6'">
-        <v-col :cols="6">
+      <!-- <v-row :class="'mb-6'">
+        <v-col :cols="6"> -->
             <h1 class=text >The first player is normally the player to the left of the dealer (you can also choose the youngest player) and gameplay usually follows a clockwise direction. Every player views his/her cards and tries to match the card in the Discard Pile.
                 You have to match either by the number, color, or the symbol/Action. For instance, if the Discard Pile has a red card that is an 8 you have to place either a red card or a card with an 8 on it. You can also play a Wild card (which can alter current color in play).
                 If the player has no matches or they choose not to play any of their cards even though they might have a match, they must draw a card from the Draw pile. If that card can be played, play it. Otherwise, keep the card, and the game moves on to the next person in turn. You can also play a Wild card, or a Wild Draw Four card on your turn.
@@ -12,8 +12,8 @@
                 The game continues until a player has one card left. The moment a player has just one card they must yell “UNO!”. If they are caught not saying “Uno” by another player before the next player has taken their turn, that player must draw two new cards as a penalty. Assuming that the player is unable to play/discard their last card and needs to draw, but after drawing, is then able to play/discard that penultimate card, the player has to repeat the action of calling out “Uno”. The bottom line is – Announcing “Uno” needs to be repeated every time you are left with one card.
                 Once a player has no cards remaining, the game round is over, points are scored, and the game begins over again. Normally, everyone tries to be the first one to achieve 500 points, but you can also choose whatever points number to win the game, as long as everyone agrees to it.
             </h1>
-        </v-col>
-        </v-row>
+        <!-- </v-col>
+        </v-row> -->
         </v-container>
     </div>        
 </template>
