68f9aa622997426862484b952547750dc2bbd581
Brady Svedin <brady.svedin@aggiemail.usu.edu>
2020-07-28 21:17:04 -0600

    Fixed up the drawCard function to use a simpler method, and proper logic on drawing a card, resetting draw pile, anc checking for proper player turn. Fixed up and expanded greatly on the unit test TestDrawCard.

--- server/uno_test.go
+++ server/uno_test.go
@@ -8,45 +8,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// test to make sure the correct colors are being returned from RandColor
-// // this test is not super helpful - it is being used to get used to unit testing in golang
-// func TestRandColor(t *testing.T) {
-// 	assert.Equal(t, randColor(0), "red")
-// 	assert.Equal(t, randColor(1), "blue")
-// 	assert.Equal(t, randColor(2), "green")
-// 	assert.Equal(t, randColor(3), "yellow")
-// 	assert.Equal(t, randColor(4), "")
-// }
+// This function is meant to get a game and a player into the data base in a usable state for testing.
+func setupGameWithPlayer(database *db.DB) (*model.Game, *model.Player) {
 
-// func TestContains(t *testing.T) {
-// 	// setup test data
-// 	var data []string = []string{}
-// 	data = append(data, "Test")
+	game, _ := database.CreateGame()
 
-// 	// check to see if "Test" is found in the correct index
-// 	index, found := contains(data, "Test")
-// 	assert.Equal(t, index, 0)
-// 	assert.Equal(t, found, true)
+	player, _ := database.CreatePlayer("Player 1")
 
-// 	// make sure a missing string is not found at any index
-// 	index, found = contains(data, "Invalid entry")
-// 	assert.Equal(t, index, -1)
-// 	assert.Equal(t, found, false)
-// }
+	game, _ = database.JoinGame(game.ID, player.ID)
+
+	game.DrawPile = generateShuffledDeck()
+
+	database.SaveGame(*game)
+
+	return game, player
+}
 
 // Test that you can get the index for the location of the card that the player holds.
 func TestCardFromPlayer(t *testing.T) {
 
-	card := model.Card("Red", "One")
-	player := model.Player("ID 1", "Player 1", []model.Card{card})
+	card := model.Card{Color: "Red", Value: "One"}
+	player := model.Player{ID: "ID 1", Name: "Player 1", Cards: []model.Card{card}}
 
 	// Test for a card the user is Guarenteed to have.
-	index := cardFromPlayer(player, card)
+	index := cardFromPlayer(&player, &card)
 
 	assert.Equal(t, index, 0)
 
 	//Test a card that the user does not have. Guaranteed this by using invalid values for the card
-	index = cardFromPlayer(player, model.Card("orange", "whoops"))
+	index = cardFromPlayer(&player, &model.Card{Color: "orange", Value: "whoops"})
 
 	assert.Equal(t, index, -1)
 }
@@ -57,57 +47,79 @@ func TestDrawCardHelper(t *testing.T) {
 
 	game, player := setupGameWithPlayer(database)
 
-	drawCardHelper(game.ID, player)
+	drawCardHelper(game, player)
 
 	assert.Equal(t, len(player.Cards), 1)
 }
 
-func TestGetPlayer(t *testing.T) {
+func TestDrawCard(t *testing.T) {
 
 	database, _ := db.GetDb()
 
 	game, player := setupGameWithPlayer(database)
 
-	player = getPlayer(game, player.ID)
+	// Test Drawing a card with a full deck and real player
+	game, err := drawCard(game.ID, player.ID)
+	game, _ = database.LookupGameByID(game.ID)
+	player = &game.Players[game.CurrentPlayer]
 
-	assert.NotNil(t, player)
+	// Assert that no error occured, the player has a new card and the draw pile
+	// has one less card
+	assert.Nil(t, err, "Failed to draw card.")
+	assert.Equal(t, 1, len(player.Cards))
+	assert.Equal(t, 107, len(game.DrawPile))
 
-	player = getPlayer(game, "Invalid ID")
-
-	assert.Nil(t, player)
-}
-
-// This function is meant to get a game and a player into the data base in a usable state for testing.
-func setupGameWithPlayer(database *db.DB) (*model.Game, *model.Player) {
-
-	game, _ := database.CreateGame()
-
-	player, _ := database.CreatePlayer("Player 1")
-
-	database.JoinGame(game.ID, player.ID)
-
-	game.DrawPile = generateShuffledDeck()
+	// Move all cards into the discard pile, Empty out the draw pile completely,
+	// and test drawing a card. It should resuffle leaving one card on the discard pile
+	game.DiscardPile = append(game.DiscardPile, game.DrawPile...)
+	game.DrawPile = game.DrawPile[:0]
+	lastCard := game.DiscardPile[len(game.DiscardPile)-1]
 
 	database.SaveGame(*game)
-	return game, player
-}
 
-func TestDrawCard(t *testing.T) {
+	game, err = drawCard(game.ID, player.ID)
+	player = &game.Players[game.CurrentPlayer]
 
-	// database, _ := db.GetDb()
+	//Assert no error, player has 2 cards from both draw tests,
+	// draw is missing three from two draws and one in discard
+	// discard has one last remaining card.
+	// Assert last card in discard is actually to proper last card
+	assert.Nil(t, err, "Failed to draw card.")
+	assert.Equal(t, 2, len(player.Cards))
+	assert.Equal(t, 105, len(game.DrawPile))
+	assert.Equal(t, 1, len(game.DiscardPile))
+	assert.Equal(t, lastCard.Color, game.DiscardPile[0].Color)
+	assert.Equal(t, lastCard.Value, game.DiscardPile[0].Value)
 
-	// game, player := setupGameWithPlayer(database)
+	// Empty out both discard and draw piles, it should for now put a new deck on the draw pile.
+	game.DrawPile = game.DrawPile[:0]
+	game.DiscardPile = game.DiscardPile[:1]
+	lastCard = game.DiscardPile[len(game.DiscardPile)-1]
 
-	// game, err := drawCard(game.ID, player.ID)
+	database.SaveGame(*game)
 
-	// if err == nil {
-	// 	player, _ = database.LookupPlayer(player.ID)
-	// 	assert.Equal(t, len(player.Cards), 1)
-	// 	assert.Equal(t, len(game.DrawPile), 107)
-	// } else {
-	// 	assert.Fail(t, "Failed to draw card.")
-	// }
-	assert.Equal(t, true, true)
+	game, err = drawCard(game.ID, player.ID)
+	player = &game.Players[game.CurrentPlayer]
+
+	// Assert no errors, assert player now has 3 cards
+	// assert new draw pile with one missing
+	// assert discard still has one card
+	// Assert last card in discard is actually to proper last card
+	assert.Nil(t, err, "Failed to draw card.")
+	assert.Equal(t, 3, len(player.Cards))
+	assert.Equal(t, 107, len(game.DrawPile))
+	assert.Equal(t, 1, len(game.DiscardPile))
+	assert.Equal(t, lastCard.Color, game.DiscardPile[0].Color)
+	assert.Equal(t, lastCard.Value, game.DiscardPile[0].Value)
+
+	// Create a player to simulate one playing but its not their turn and attempt to play
+	otherPlayer := model.Player{ID: " id 2 ", Name: "Name 2", Cards: []model.Card{}}
+
+	_, err = drawCard(game.ID, otherPlayer.ID)
+
+	// Assert that we got an error from the draw card.
+	assert.NotNil(t, err, "Player played out of turn. Please make sure only the player whoes turn it is can play.")
+
 }
 
 /*func TestPlayCard(t *testing.T) {
