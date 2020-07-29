a1493076a2ee9c4f6102782ee068e0c3e9d77a74
Jacob Christensen <jacob.h.christensen@gmail.com>
2020-07-28 19:05:28 -0600

    Updates to make the game state blank the other players cards

--- server/routeHandlers.go
+++ server/routeHandlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/jak103/uno/model"
 	"github.com/labstack/echo/v4"
 )
@@ -11,7 +12,7 @@ var sim bool = true
 
 type Response struct {
 	ValidGame bool                   `json:"valid"` // Valid game id
-	Payload   map[string]interface{} `json:"payload"`
+	GameState map[string]interface{} `json:"payload"`
 }
 
 func setupRoutes(e *echo.Echo) {
@@ -30,7 +31,7 @@ func newGame(c echo.Context) error {
 		return err
 	}
 
-	return c.JSONPretty(http.StatusOK, &Response{true, newPayload(game)}, "  ")
+	return c.JSONPretty(http.StatusOK, &Response{true, getGameState(game)}, "  ")
 }
 
 func login(c echo.Context) error {
@@ -40,7 +41,7 @@ func login(c echo.Context) error {
 		return err
 	}
 
-	return c.JSONPretty(http.StatusOK, &Response{true, newPayload(game)}, "  ")
+	return c.JSONPretty(http.StatusOK, &Response{true, getGameState(game)}, "  ")
 }
 
 func startGame(c echo.Context) error {
@@ -49,15 +50,17 @@ func startGame(c echo.Context) error {
 }
 
 func update(c echo.Context) error {
+	playerID := getPlayerFromContext(c)
 	game, err := updateGame(c.Param("game"), c.Param("username"))
 	if err != nil {
 		return err
 	}
-	return c.JSONPretty(http.StatusOK, &Response{true, newPayload(game)}, "  ")
+	return c.JSONPretty(http.StatusOK, &Response{true, getGameState(game, playerID)}, "  ")
 }
 
 func play(c echo.Context) error {
 	// TODO Cards have a value, which can include skip, reverse, etc
+	playerID := getPlayerFromContext(c)
 	card := model.Card{c.Param("number"), c.Param("color")}
 	game, err := playCard(c.Param("game"), c.Param("username"), card)
 
@@ -65,31 +68,51 @@ func play(c echo.Context) error {
 		return err
 	}
 
-	return c.JSONPretty(http.StatusOK, &Response{true, newPayload(game)}, "  ")
+	return c.JSONPretty(http.StatusOK, &Response{true, getGameState(game, playerID)}, "  ")
 }
 
 func draw(c echo.Context) error {
+	playerID := getPlayerFromContext(c)
 	game, err := drawCard(c.Param("game"), c.Param("username"))
 
 	if err != nil {
 		return err
 	}
 
-	return c.JSONPretty(http.StatusOK, &Response{true, newPayload(game)}, "  ")
+	return c.JSONPretty(http.StatusOK, &Response{true, getGameState(game, playerID)}, "  ")
 
 }
 
-func newPayload(game *model.Game) map[string]interface{} {
-	payload := make(map[string]interface{})
+func getGameState(game *model.Game, playerID string) map[string]interface{} {
+	gameState := make(map[string]interface{})
 
 	// Update known variables
-	payload["direction"] = game.Direction
-	payload["current_player"] = game.CurrentPlayer
-	payload["all_players"] = game.Players
-	payload["draw_pile"] = game.DrawPile
-	payload["discard_pile"] = game.DiscardPile
-	payload["game_id"] = game.ID
-	payload["game_over"] = game.Status == "Finished"
+	gameState["direction"] = game.Direction
+	gameState["current_player"] = game.CurrentPlayer
+	gameState["draw_pile"] = game.DrawPile
+	gameState["discard_pile"] = game.DiscardPile
+	gameState["game_id"] = game.ID
+	gameState["game_over"] = (game.Status == "Finished")
 
-	return payload
+	for _, player := range game.Players {
+		if player.ID != playerID {
+			for _, card := range player.Cards {
+				card.Color = "Blank"
+				card.Value = "Blank"
+			}
+		}
+	}
+
+	gameState["all_players"] = game.Players
+
+	return gameState
+}
+
+func getPlayerFromContext(c echo.Context) string {
+	// TODO Update this to the actual claim key once the JWT team is done
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	playerID := claims["playerID"].(string)
+
+	return playerID
 }
