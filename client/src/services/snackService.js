abae8f8b244fb68eeb5286922423ca7bd2ef2356
Kimberly <klindholm7@gmail.com>
2020-08-02 18:54:38 -0700

    snackService.js added.

---
+++ client/src/services/snackService.js
@@ -0,0 +1,8 @@
+import BaseService from "./baseService";
+
+export default {
+    async sendMessage(gameID, message, showSnack) {
+        //console.log('snack status:', showSnack);
+        return BaseService.post(`/api/games/${gameID}/sendMessage`, {value: message, showSnack});
+    },
+}
