902595deb3f1de72d6d02ddae5eb319e7d1146bd
Andrew McMullin <mcmullinboy15@gmail.com>
2020-08-03 13:33:25 -0600

    I fixed the anchor tags and got them working

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -33,6 +33,14 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
+  // for anchor scrolling
+  scrollBehavior (to) {
+    if (to.hash) {
+        return {selector: to.hash}
+    } else {
+        return { x: 0, y: 0 }
+    }
+  },
   routes
 })
 
